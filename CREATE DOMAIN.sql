b3132c3e61121f6259cb2f01a502c9ea60d308d9
gennooo <157842567+gennooo@users.noreply.github.com>
2024-10-16 16:27:08 +0200

    Update CREATE DOMAIN.sql

--- CREATE DOMAIN.sql
+++ CREATE DOMAIN.sql
@@ -1,3 +1,3 @@
-CREATE DOMAIN ruolo_position AS VARCHAR(30) CHECK (VALUE IN ('Portiere', 'Difensore', 'Centrocampista', 'Attaccante'));
+CREATE DOMAIN ruolo_position AS VARCHAR(15) CHECK (VALUE IN ('Portiere', 'Difensore', 'Centrocampista', 'Attaccante'));
 
 CREATE DOMAIN id_valido AS VARCHAR(9) CHECK (VALUE SIMILAR TO '[0-9]{4}-[0-9]{4}');
